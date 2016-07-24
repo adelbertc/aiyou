0254c10382281e7a144c0cdac3569ab5cf5ace23
Adelbert Chang <adelbertc@gmail.com>
2016-07-23 23:25:12 -0700

    Mark IORef var volatile

--- yax/core/src/main/scala/io/IORef.scala
+++ yax/core/src/main/scala/io/IORef.scala
@@ -1,12 +1,11 @@
 package io
 
-sealed abstract class IORef[A] {
-  protected var value: A
-
+final class IORef[A] private(@volatile protected var value: A) {
   def read: IO[A] = IO.primitive(value)
 
   def write(a: A): IO[Unit] = IO.primitive(value = a)
 
+  /** Modify the value behind the reference with the function. Please note that modify is not an atomic operation. */
   def modify(f: A => A): IO[Unit] = for {
     a <- read
     _ <- write(f(a))
@@ -14,5 +13,5 @@ sealed abstract class IORef[A] {
 }
 
 object IORef {
-  def create[A](a: A): IO[IORef[A]] = IO.primitive(new IORef[A] { protected var value = a })
+  def create[A](a: A): IO[IORef[A]] = IO.primitive(new IORef(a))
 }
